a74fead09f95457aa815e0adb7bd2d66ee8cb4f8
vishal <vishal@gmail.com>
2023-07-03 17:48:19 +0530

    task_10_dynamic_route_part3

---
+++ express/task__10__dynamic__route__part__3/routes/admin.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+const express = require("express");
+
+const adminController = require("../controllers/admin");
+
+const router = express.Router();
+
+// GET - /add-product
+router.get("/add-product", adminController.getAddProduct);
+
+// GET - /products
+router.get("/admin-product", adminController.getAdminProducts);
+
+// POST - /add-product
+router.post("/add-product", adminController.postAddProduct);
+
+// GET  - /edit-product/34
+router.get("/edit-product/:productId", adminController.getEditMyProduct);
+
+// POST- /edit-product
+router.post("/edit-product", adminController.saveModifedProduct);
+//POST - /remove-product
+router.post("/remove-product", adminController.removeProduct);
+
+module.exports = router;
